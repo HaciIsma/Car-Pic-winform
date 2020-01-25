f19713d2b17c41b9f7fb93bdfcf534006a8bb735
HaciIsma <haciismayilov5@outlook.com>
2020-01-25 18:17:04 +0400

    second commit

---
+++ Car.cs
@@ -0,0 +1,11 @@
+﻿namespace WindowsFormsApp18
+{
+    class Car
+    {
+        public int Year { get; set; }
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string CarImage { get; set; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp18
+{
+    public partial class Form1 : Form
+    {
+        static int index = default;
+        List<Car> cars = new List<Car>();
+        public Form1()
+        {
+            InitializeComponent();
+            cars.Add(new Car { Vendor = "BMW", Model = "X5", Color = "Black", Year = 2014, CarImage = $@"{Directory.GetCurrentDirectory() + "X5.jpg"}" });
+            //pictureBox1.ImageLocation = @"C:\Users\Haci\source\repos\WindowsFormsApp18\WindowsFormsApp18\bin\Debug\CarPictures\BMW-X5.jpg";
+            MessageBox.Show(Directory.GetCurrentDirectory());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // if  cars.Count()  = max;
+            pictureBox1.ImageLocation = cars[0].CarImage;
+            //index++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // if (0)
+            index--;
+        }
+    }
+}
