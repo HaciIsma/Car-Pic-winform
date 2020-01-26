e397e465cd79f817d98e0060e55ddec03741373b
HaciIsma <haciismayilov5@outlook.com>
2020-01-27 00:38:14 +0400

    second commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,110 +29,111 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.prev = new System.Windows.Forms.Button();
+            this.model = new System.Windows.Forms.Label();
+            this.vendor = new System.Windows.Forms.Label();
+            this.year = new System.Windows.Forms.Label();
+            this.color = new System.Windows.Forms.Label();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // next
             // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.MediumBlue;
-            this.button1.Location = new System.Drawing.Point(684, 341);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(62, 55);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Next";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next.ForeColor = System.Drawing.Color.MediumBlue;
+            this.next.Location = new System.Drawing.Point(684, 341);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(62, 55);
+            this.next.TabIndex = 0;
+            this.next.Text = "Next";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // prev
             // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.MediumBlue;
-            this.button2.Location = new System.Drawing.Point(616, 341);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(62, 55);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Prev";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.prev.BackColor = System.Drawing.Color.Transparent;
+            this.prev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.prev.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prev.ForeColor = System.Drawing.Color.MediumBlue;
+            this.prev.Location = new System.Drawing.Point(616, 341);
+            this.prev.Name = "prev";
+            this.prev.Size = new System.Drawing.Size(62, 55);
+            this.prev.TabIndex = 1;
+            this.prev.Text = "Prev";
+            this.prev.UseVisualStyleBackColor = false;
+            this.prev.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // model
+            // 
+            this.model.AutoSize = true;
+            this.model.Location = new System.Drawing.Point(430, 40);
+            this.model.Name = "model";
+            this.model.Size = new System.Drawing.Size(10, 13);
+            this.model.TabIndex = 3;
+            this.model.Text = " ";
+            // 
+            // vendor
+            // 
+            this.vendor.AutoSize = true;
+            this.vendor.Location = new System.Drawing.Point(430, 66);
+            this.vendor.Name = "vendor";
+            this.vendor.Size = new System.Drawing.Size(10, 13);
+            this.vendor.TabIndex = 4;
+            this.vendor.Text = " ";
+            // 
+            // year
+            // 
+            this.year.AutoSize = true;
+            this.year.Location = new System.Drawing.Point(430, 91);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(10, 13);
+            this.year.TabIndex = 5;
+            this.year.Text = " ";
+            // 
+            // color
+            // 
+            this.color.AutoSize = true;
+            this.color.Location = new System.Drawing.Point(430, 117);
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(10, 13);
+            this.color.TabIndex = 6;
+            this.color.Text = " ";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(430, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Model: ";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(430, 66);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Vendor: ";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(430, 91);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Year:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(430, 117);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Color";
-            // 
-            // errorProvider1
-            // 
-            this.errorProvider1.ContainerControl = this;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.color);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.vendor);
+            this.Controls.Add(this.model);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.prev);
+            this.Controls.Add(this.next);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,14 +141,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button next;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button prev;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label model;
+        private System.Windows.Forms.Label vendor;
+        private System.Windows.Forms.Label year;
+        private System.Windows.Forms.Label color;
         private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,39 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp18
 {
     public partial class Form1 : Form
     {
-        static int index = default;
+        static int index = -1;
         List<Car> cars = new List<Car>();
+        string path = $@"{Directory.GetCurrentDirectory()}\Images\";
         public Form1()
         {
             InitializeComponent();
-            cars.Add(new Car { Vendor = "BMW", Model = "X5", Color = "Black", Year = 2014, CarImage = $@"{Directory.GetCurrentDirectory() + "X5.jpg"}" });
-            //pictureBox1.ImageLocation = @"C:\Users\Haci\source\repos\WindowsFormsApp18\WindowsFormsApp18\bin\Debug\CarPictures\BMW-X5.jpg";
-            MessageBox.Show(Directory.GetCurrentDirectory());
+            cars.Add(new Car { Vendor = "BMW", Model = "X5", Color = "Black", Year = 2014, CarImage = path + "X5.png" });
+            cars.Add(new Car { Vendor = "Mercedes", Model = "GT", Color = "Yellow", Year = 2020, CarImage = path + "GT.png" });
+            cars.Add(new Car { Vendor = "Audi", Model = "S4", Color = "Yellow", Year = 2009, CarImage = path + "S4.png" });
+            cars.Add(new Car { Vendor = "Explorer", Model = "Ford", Color = "Silver", Year = 2011, CarImage = path + "Explorer.png" });
+            cars.Add(new Car { Vendor = "Fit", Model = "Honda", Color = "Red", Year = 2016, CarImage = path + "Fit.png" });
+            cars.Add(new Car { Vendor = "6", Model = "Mazda", Color = "Blue", Year = 2016, CarImage = path + "6.png" });
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // if  cars.Count()  = max;
-            pictureBox1.ImageLocation = cars[0].CarImage;
-            //index++;
+            next.Enabled = true;
+            prev.Enabled = true;
+            if (index != cars.Count() - 1)
+            {
+                index++;
+                PictureChangeAndInfo(index);
+            }
+            else
+            {
+                next.Enabled = false;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // if (0)
-            index--;
+            next.Enabled = true;
+            prev.Enabled = true;
+            if (index > 0)
+            {
+                index--;
+                PictureChangeAndInfo(index);
+            }
+            else
+            {
+                prev.Enabled = false;
+            }
         }
+
+        private void PictureChangeAndInfo(int index)
+        {
+            pictureBox1.Image = Image.FromFile(cars[index].CarImage);
+            model.Text = $"Model: {cars[index].Model}";
+            vendor.Text = $"Vendor: {cars[index].Vendor}";
+            year.Text = $"Year: {cars[index].Year}";
+            color.Text = $"Color: {cars[index].Color}";
+        }
+
     }
 }
